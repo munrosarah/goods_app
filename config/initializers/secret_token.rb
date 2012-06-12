910c39155bd421ef3e2f4275c879c6b03485b090
Sarah Munro <munro.sarah@gmail.com>
2012-06-11 20:10:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodsApp::Application.config.secret_token = 'afb527cf3b069ae4eaf7c7bbdd005e7577620a752c8ea2869e622a66ca14d9cb6d327130dfe0523eac439aa39d51f890d7a2e4d3fe17a22f777de6859a0dc5d0'
